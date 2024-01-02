fd4504f6653cd90d787aef4610fdb16ad3273ebb
Tim Rolshud <tim.rolsh@gmail.com>
2024-01-02 16:51:03 -0500

    capital added but lowercase not deleted

--- extension.js
+++ extension.js
@@ -1,38 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const editor = vscode.window.activeTextEditor;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
     let disposable = vscode.workspace.onDidChangeTextDocument((event) => {
-
-
-
-
-        console.log("changed")
-        // const editor = vscode.window.activeTextEditor;
-        // if (editor && editor.document.languageId === "markdown") {
-        //     const changes = event.contentChanges;
-        //     changes.forEach((change) => {
-        //         if (change.text === "$") {
-        //             const position = change.range.start;
-        //             editor.edit((editBuilder) => {
-        //                 editBuilder.insert(position.translate(0, 1), "$");
-        //             });
-        //         }
-        //     });
-        // }
+        if (editor && event.document === editor.document) {
+            const changes = event.contentChanges;
+            if (changes.length > 0) {
+                const change = changes[0];
+                handleTextChange(change);
+            }
+        }
     });
 
     context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function handleTextChange(change) {
+    const newText = change.text;
+    const startPosition = change.range.start;
+    const lineText = editor.document.lineAt(startPosition.line).text;
+
+    if (
+        newText.length === 1 &&
+        isLowerCase(newText) &&
+        startPosition.character >= 2
+    ) {
+        const precedingChars = lineText.substring(
+            startPosition.character - 2,
+            startPosition.character
+        );
+        if (precedingChars === "* " || precedingChars === "- ") {
+            const capitalized = newText.toUpperCase();
+            const range = new vscode.Range(startPosition, change.range.end);
+
+            editor.edit((editBuilder) => {
+                editBuilder.replace(range, capitalized);
+            });
+        }
+    }
+}
+
+function isLowerCase(str) {
+    return str === str.toLowerCase() && str !== str.toUpperCase();
+}
+
 function deactivate() {}
 
 module.exports = {
